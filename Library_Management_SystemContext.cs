6c37815c0c4df0baccb2a27c50db797fb55cc06d
Sunil579 <153346075+Sunil579@users.noreply.github.com>
2024-01-25 13:31:55 +0545

    Add files via upload

---
+++ Library_Management_SystemContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Library.Data
+{
+        internal class Library_Management_SystemContext
+    {
+
+        public class LibraryManagementSystemContext : DbContext
+        {
+            public LibraryManagementSystemContext() : base()
+            { }
+
+            public DbSet<Library.Models.Book> Books { get; set; }
+
+            public DbSet<Library.Models.Librarian> Librarians { get; set; }
+
+            public DbSet<Library.Models.Report> Reports { get; set; }
+
+            public DbSet<Library.Models.Transaction> Transactions { get; set; }
+
+            public DbSet<Library.Models.Students> Students { get; set; }
+
+            public DbSet<Library.Models.Borrowing> Borrowings { get; set; }
+        }
+    }
+}
